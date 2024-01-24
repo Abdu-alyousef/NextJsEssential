316260d0c9487b0a3439bf31183b37f41444f269
Abdu-alyousef <boud.yousef95@gmail.com>
2024-01-24 10:44:26 +0100

    fetching the Data from sqlite3 database

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,13 @@
 import Link from "next/link"
 import classes from "./page.module.css"
 import MealsGrid from "@/components/meals/meals-grid"
-export default function MealsPage () {
+
+import { getMeals } from '@/lib/meals.js';
+
+export default  async function MealsPage() {
+  const meals = await getMeals();
+
+
     return (
         <>
         <header className={classes.header}>
@@ -12,7 +18,7 @@ export default function MealsPage () {
             </p>
         </header>
         <main className={classes.main}>
-            <MealsGrid meals={[]} />
+            <MealsGrid meals={meals} />
         </main>
         </>
     )
